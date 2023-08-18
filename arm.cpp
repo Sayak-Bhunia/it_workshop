bbe20f0224128c20765f472c9ac16adc45ac9362
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-08-18 16:55:27 +0530

    Add files via upload

---
+++ arm.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+public:
+    int id;
+    string name;
+};
+
+int main()
+{
+    Student s1,s2;
+    s1.id = 31;
+    s2.id = 32;
+    s1.name = "Sayak Bhunia";
+    s2.name = "Shounak Sinha";
+    cout << s1.id <<endl;
+    cout << s1.name <<endl;
+    cout << s2.id <<endl;
+    cout << s2.name <<endl;
+};
