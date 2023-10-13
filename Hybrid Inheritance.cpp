4516c0ef682ae9b4563cf9b2ed276604a96962e0
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-10-13 22:31:30 +0530

    Create Hybrid Inheritance.cpp

---
+++ Hybrid Inheritance.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class A
+{
+protected:
+    int a;
+public:
+    void geta()
+    {
+        cout << "Enter the value of a: " <<endl;
+        cin >> a;
+    }
+};
+
+class B:public A
+{
+protected:
+    int b;
+public:
+    void getb()
+    {
+        cout << "Enter the value of b: " <<endl;
+        cin >> b;
+    }
+};
+
+class C
+{
+protected:
+    int c;
+public:
+    void getc()
+    {
+        cout << "Enter the value of c is:" <<endl;
+        cin >> c;
+    }
+};
+
+class D:public B,public C
+{
+protected:
+    int d;
+public:
+    void mul()
+    {
+        geta();
+        getb();
+        getc();
+        cout << "Multiplication of a,b,c is: " << a*b*c <<endl;
+    }
+};
+
+int main()
+{
+    D d;
+    d.mul();
+}
