5329374798c1dda1b8e49bcdaae99bd780b8f49c
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-10-13 22:29:35 +0530

    Create Ambiguity resolution in Inheritance.cpp

---
+++ Ambiguity resolution in Inheritance.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class A
+{
+public:
+    void display()
+    {
+        cout << "Class A" <<endl;
+    }
+};
+
+class B
+{
+public:
+    void display()
+    {
+        cout << "Class B" <<endl;
+    }
+};
+
+class C:public A,public A
+{
+    void view()
+    {
+        display();
+    }
+};
+
+int main()
+{
+    C c;
+    c.display();
+}
