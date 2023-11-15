49295d2478280e5eeb88165844061397e005b76e
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-11-15 18:19:01 +0530

    Create Operator_Overloading2.cpp

---
+++ Operator_Overloading2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Complex
+{
+private:
+    int real,img;
+public:
+    Complex(int r=0, int i=0) {
+        this->real = r;
+        this->img = i;
+    }
+    Complex operator + (Complex const &obj) { /* &obj is the address of the objects that contain the complex and real numbers */
+        Complex res; /* obj of the complex class for using outside main */
+        res.real = this->real + obj.real; /* here "this->" just acts like an indicator old conventional method */
+        res.img = this->img + obj.img;
+        return res;
+    }
+    void print() {
+        cout << real << "+i" <<img <<endl;
+    }
+};
+
+int main()
+{
+    Complex c1(2,3),c2(3,5);
+    Complex c3 = c1 + c2;
+    c3.print();
+}
